1cb68a72ec99af733aac2ca08b2b71ecccd666bd
Arthur Ming <937765518@qq.com>
2017-06-30 20:54:50 +0800

    finish basic features

--- MusicPlayer/Pages/SongPlayPage.xaml.cs
+++ MusicPlayer/Pages/SongPlayPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -37,6 +38,9 @@ namespace MusicPlayer.Pages
 
         private void SongPlayPage_SizeChanged(object sender, SizeChangedEventArgs e)
         {
+            this.MediaPlayer.Width = e.NewSize.Width - 12;
+            this.MediaPlayer.Height = this.MediaGrid.ActualHeight;
+            System.Diagnostics.Debug.WriteLine("this.MediaGrid.ActualHeight : {0}", this.MediaGrid.ActualHeight);
             this.PlayerPostion.Width = e.NewSize.Width - 12;
         }
 
@@ -61,6 +65,8 @@ namespace MusicPlayer.Pages
             {
                 currentSong = song;
                 PlaySongInternal(currentSong);
+                this.BttnPlay.Visibility = Visibility.Visible;
+                this.BttnStop.Visibility = Visibility.Collapsed;
             }
             else
             {
@@ -78,10 +84,62 @@ namespace MusicPlayer.Pages
             else
             {
                 this.MediaPlayer.PosterSource = song.Thumbnail;
-            }
-            this.BttnPlay.Visibility = Visibility.Visible;
-            this.BttnStop.Visibility = Visibility.Collapsed;
+            }        
             this.MediaPlayer.Play();
+        }
+
+        private void PlaySongInternal()
+        {
+            if (IsSongSelected())
+            {
+                PlaySongInternal(currentSong);
+            }
+        }
+
+        private void StopSongInternal()
+        {
+            this.MediaPlayer.Pause();
+        }
+
+
+        private void BttnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            StopSongInternal();
+            this.BttnPlay.Visibility = Visibility.Collapsed;
+            this.BttnStop.Visibility = Visibility.Visible;
+
+        }
+
+        private void BttnStop_Click(object sender, RoutedEventArgs e)
+        {
+            if(IsSongSelected())
+            {
+                PlaySongInternal(currentSong);
+                this.BttnPlay.Visibility = Visibility.Visible;
+                this.BttnStop.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                ShowErorAsync("请选择歌曲");
+            }
+        }
+
+        private void BttnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsSongSelected())
+            {
+                GetPreviousSong();
+                PlaySongInternal();
+            }
+        }
+
+        private void BttnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsSongSelected())
+            {
+                GetNextSong();
+                PlaySongInternal();
+            }
         }
 
         private void MediaPlayer_MediaOpened(object sender, RoutedEventArgs e)
@@ -94,25 +152,57 @@ namespace MusicPlayer.Pages
         {
             if (this.SwtchMediaRepeat.IsOn)
             {
-                IList<Song> songs = MyMusicPage.Current.Songs;
-                int index = songs.IndexOf(currentSong) + 1;
-                if(index == songs.Count)
-                {
-                    index = 0;
-                }
-                currentSong = songs[index];
+                GetNextSong();
             }         
             PlaySongInternal(currentSong);            
         }
 
-        private void BttnPlay_Click(object sender, RoutedEventArgs e)
+        private void GetNextSong()
         {
-
+            IList<Song> songs = MyMusicPage.Current.Songs;
+            int index = songs.IndexOf(currentSong) + 1;
+            if (index == songs.Count)
+            {
+                index = 0;
+            }
+            currentSong = songs[index];
         }
+
+        private void GetPreviousSong()
+        {
+            IList<Song> songs = MyMusicPage.Current.Songs;
+            int index = songs.IndexOf(currentSong) - 1;
+            if (index == -1)
+            {
+                index = songs.Count -1;
+            }
+            currentSong = songs[index];
+        }
+
 
         private void SldrVolume_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
             this.MediaPlayer.Volume = this.SldrVolume.Value / 100.0;
         }
+
+        private async void ShowErorAsync(string message)
+        {
+            MessageDialog md = new MessageDialog(message);
+            await md.ShowAsync();
+        } 
+
+        private bool IsSongSelected()
+        {
+            if(currentSong == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+     
     }
 }
